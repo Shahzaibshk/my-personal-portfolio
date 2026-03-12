4c0a7686733e6a72f610f9054891131e15b5c32c
Shahzaib Ali <ishahzaibali1472@gmail.com>
2026-03-13 01:10:31 +0500

    fix vercel build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,33 @@
+# #!/bin/bash
+
+# # Install Flutter
+# git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+# export PATH="$PATH:`pwd`/flutter/bin"
+
+# flutter doctor
+# flutter config --enable-web
+
+# flutter pub get
+# flutter build web
+
+
 #!/bin/bash
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
+set -e  # stop on error
 
-flutter doctor
+echo "Installing Flutter..."
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+
+export PATH="$PATH:$HOME/flutter/bin"
+
+flutter --version
 flutter config --enable-web
 
+echo "Getting packages..."
 flutter pub get
-flutter build web
+
+echo "Building Flutter Web..."
+flutter build web --release
+
+echo "Build completed!"
