7f76956e0ea92d4524b3d4de9f503ae66775f7e0
Shahzaib Ali <ishahzaibali1472@gmail.com>
2026-03-13 01:06:46 +0500

    fix: vercel flutter build

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter doctor
+flutter config --enable-web
+
+flutter pub get
+flutter build web
